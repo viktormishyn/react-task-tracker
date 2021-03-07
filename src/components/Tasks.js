64b145663e6bdb57b7b7fd5dbe71f48c5a7586f0
Viktor Mishyn <viktormishyn@gmail.com>
2021-03-07 19:57:05 +0200

    Create state. Create Task.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,16 @@
-const tasks = [{
-    "id": 1,
-    "text": "Doctors Appointment",
-    "day": "Feb 5th at 2:30pm",
-    "reminder": true
-},
-    {
-        "id": 2,
-        "text": "Meeting at School",
-        "day": "Feb 6th at 1:30pm",
-        "reminder": true
-    }
-]
+import Task from "./Task";
 
-const Tasks = () => {
+const Tasks = ({tasks}) => {
+
     return (
         <>
-            {tasks.map(
-                (task) =>
-                (<h3 key={task.id}>{task.text}</h3>)
-            )
-            }
+            {tasks.map((task) => (
+                <Task key={task.id} task={task}/>
+            ))}
         </>
     )
 }
 
 export default Tasks
+
+// setTasks([...tasks, {"id": 3, "text": "Kill Bill", "day": "Mar 1st at 00:00 am", "reminder": true}])
